841ecc919d12f94e3d1865f3919b6d96265a51a0
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-22 16:36:13 +0500

    Refactor userController.js for improved readability and maintainability

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,7 @@ const register = async (req, res) => {
 
     if (!fullname || !email || !phoneNumber || !password || !role) {
       return res.status(400).json({
-        message: "Something is missing",
+        message: "Incomplete input.",
         success: false,
       });
     }
@@ -21,7 +21,7 @@ const register = async (req, res) => {
     const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({
-        message: "User already exist with this email.",
+        message: "User already exists.",
         success: false,
       });
     }
@@ -39,20 +39,21 @@ const register = async (req, res) => {
     });
 
     return res.status(201).json({
-      message: "Account created successfully.",
+      message: "Account has been  created successfully.",
       success: true,
     });
   } catch (error) {
     res.status(500).json({ message: error.message, success: false });
   }
 };
+
 const login = async (req, res) => {
   try {
     const { email, password, role } = req.body;
 
     if (!email || !password || !role) {
       return res.status(400).json({
-        message: "Something is missing",
+        message: "Invalid input.",
         success: false,
       });
     }
@@ -70,21 +71,18 @@ const login = async (req, res) => {
         success: false,
       });
     }
-    // check role is correct or not
     if (role !== user.role) {
       return res.status(400).json({
         message: "Account doesn't exist with current role.",
         success: false,
       });
     }
-
     const tokenData = {
       userId: user._id,
     };
     const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
       expiresIn: "1d",
     });
-
     user = {
       _id: user._id,
       fullname: user.fullname,
@@ -93,7 +91,6 @@ const login = async (req, res) => {
       role: user.role,
       profile: user.profile,
     };
-
     return res
       .status(200)
       .cookie("token", token, {
@@ -101,15 +98,12 @@ const login = async (req, res) => {
         httpsOnly: true,
         sameSite: "strict",
       })
-      .json({
-        message: `Welcome back ${user.fullname}`,
-        user,
-        success: true,
-      });
+      .json({ message: `Welcome back ${user.fullname}`, user, success: true });
   } catch (error) {
     res.status(500).json({ message: error.message, success: false });
   }
 };
+
 const logout = async (req, res) => {
   try {
     return res.status(200).cookie("token", "", { maxAge: 0 }).json({
@@ -123,9 +117,7 @@ const logout = async (req, res) => {
 const updateProfile = async (req, res) => {
   try {
     const { fullname, email, phoneNumber, bio, skills } = req.body;
-
     const file = req.file;
-    // cloudinary ayega idhar
     const fileUri = getDataUri(file);
     const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
 
@@ -133,7 +125,7 @@ const updateProfile = async (req, res) => {
     if (skills) {
       skillsArray = skills.split(",");
     }
-    const userId = req.id; // middleware authentication
+    const userId = req.id;
     let user = await User.findById(userId);
 
     if (!user) {
@@ -142,21 +134,17 @@ const updateProfile = async (req, res) => {
         success: false,
       });
     }
-    // updating data
     if (fullname) user.fullname = fullname;
     if (email) user.email = email;
     if (phoneNumber) user.phoneNumber = phoneNumber;
     if (bio) user.profile.bio = bio;
     if (skills) user.profile.skills = skillsArray;
 
-    // resume comes later here...
     if (cloudResponse) {
-      user.profile.resume = cloudResponse.secure_url; // save the cloudinary url
-      user.profile.resumeOriginalName = file.originalname; // Save the original file name
+      user.profile.resume = cloudResponse.secure_url;
+      user.profile.resumeOriginalName = file.originalname;
     }
-
     await user.save();
-
     user = {
       _id: user._id,
       fullname: user.fullname,
@@ -165,7 +153,6 @@ const updateProfile = async (req, res) => {
       role: user.role,
       profile: user.profile,
     };
-
     return res.status(200).json({
       message: "Profile updated successfully.",
       user,
