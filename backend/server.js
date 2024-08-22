14e493eccc10a65c609300465d25b7206954cfa5
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-22 15:08:29 +0500

    Update backend server configuration and dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+const userRoute = require("./routes/userRoute.js");
+
+dotenv.config({});
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+// api's
+app.use("/api/v1/user", userRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
 });
