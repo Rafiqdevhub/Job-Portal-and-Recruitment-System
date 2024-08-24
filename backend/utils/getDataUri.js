b526c3755752e5b79ebe8f505c8b19ca885c96e5
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-24 21:18:36 +0500

    improve error handling and logging

--- backend/utils/getDataUri.js
+++ backend/utils/getDataUri.js
@@ -2,6 +2,10 @@ const DataUriParser = require("datauri/parser.js");
 const path = require("path");
 
 const getDataUri = (file) => {
+  if (!file || !file.originalname) {
+    throw new Error("Please full fill the required fields");
+  }
+
   const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
   return parser.format(extName, file.buffer);
