820aaa517a42bb5f4ba88ea933c6f91e46002239
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-25 11:42:45 +0500

    Refactor import paths and improve error handling in LatestJobCards component

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,8 +1,6 @@
 import LatestJobCards from "./LatestJobCards";
 import { useSelector } from "react-redux";
 
-// const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8];
-
 const LatestJobs = () => {
   const { allJobs } = useSelector((store) => store.job);
 
