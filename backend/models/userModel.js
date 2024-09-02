a6c538db0108fe002ea25608f0f3816794a87d4d
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-02 13:56:50 +0500

    Refactor role selection in UserModel to use "Employee" instead of "Student"

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["student", "recruiter"],
+      enum: ["employee", "recruiter"],
       required: true,
     },
     profile: {
